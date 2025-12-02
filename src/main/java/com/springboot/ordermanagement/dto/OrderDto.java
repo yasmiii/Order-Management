40a47d4289f3c9c3f1894e452b77ac0be451e906
yanaonzdev <109675600+yasmiii@users.noreply.github.com>
2025-12-02 14:45:46 +0530

    Refactor OrderDto constructors for improved clarity

--- src/main/java/com/springboot/ordermanagement/dto/OrderDto.java
+++ src/main/java/com/springboot/ordermanagement/dto/OrderDto.java
@@ -5,7 +5,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor@NoArgsConstructor
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderDto {
     private int id;
     private int itemId;
